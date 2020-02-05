826d8ffa3990a73615673eebd1f530b8bd9ad0f5
Elke Burgers <elke.burgers@hotmail.com>
2020-02-05 10:53:02 -0300

    exercicio 2 correto

--- aula_1/src/components/app.js
+++ aula_1/src/components/app.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import TableProfissionais from './table_profissionais';
 
 // para habilitar para ser importado por outras classes/ arquivos usar 'export default':
     export default class App extends React.Component {
     render() {
         return (
+            // como soh renderiza um componente, incluimos uma div porque ele entende que estah renderizando apenas a div. Sem ela, o react entende que estah renderizando o h1 e o tableProfissionais.
+            <div>
             <h1>Hello World</h1>
+            <TableProfissionais/>
+            </div>
         )
     }
 }
---
+++ aula_1/src/components/table_profissionais.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// o nome da classe TableProfissionais eh diferente do nome do arquivo porque um eh classe e outro eh estrutura de arquivos
+// o React acima (import) eh o mesmo React abaixo (component)
+    export default class TableProfissionais extends React.Component {
+    render() {
+        return (
+            <h2>Eu sou uma tabela!</h2>
+        )
+    }
+}
+
+

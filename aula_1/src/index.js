55afb009a9f95a6f2082794858a734cbae792c80
Elke Burgers <elke.burgers@hotmail.com>
2020-02-05 09:51:10 -0300

    retorno dos comentarios iniciais

--- aula_1/src/index.js
+++ aula_1/src/index.js
@@ -6,11 +6,24 @@ import "bootstrap/dist/css/bootstrap.css";
 import './index.css';
 
 // lembrar de importar o arquivo:
+// import Post from "./components/posts";
 import App from "./components/app";
 
 ReactDOM.render(
     ( 
-        <App titulo="Hello world"/>
+        // className eh a class do JS
+        // <div className="content">
+        //     <div className="card card-default">
+        //         <div className="card-header">
+        //         <h1> Hello world! </h1>
+        //         </div>
+        //    {/* 'ola mundo' eh a propriedade atribuida ao componente post criado em posts.js  */}
+            <App titulo="Hello world"/>
+        //    {/* <Post texto="Olá Mundo" titulo="Primeiro"/>
+        //    <Post texto="Eu sou o segundo post." titulo="Segundo"/>
+        //  <Post texto="Eu sou o último post." titulo="Terceiro"/> */}
+        //     </div>
+        // </div>
     ),
     document.getElementById('root')
 );

da91e9a7b69931523cf6a31f19323db0a16dc07a
Elke Burgers <elke.burgers@hotmail.com>
2020-02-07 10:27:44 -0300

    ~relogio atualiza na tela

--- clock/src/components/Clock.js
+++ clock/src/components/Clock.js
@@ -9,6 +9,16 @@ export default class Clock extends React.Component {
         }
     }
 
+    // para fazer o relógio atualizar sozinho, usa o DiMount que encerra o componente, para parar de contar em back, e setInterval para atualizar o relogio / componente
+    // 1000 milisegundos, ou seja, um segundo, para ser o interval que ele vai usar de referencia para atualizar o relogio
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                timer: new Date().toLocaleTimeString()
+            })
+        }, 1000)
+    }
+
     render() {
         return (
             <div>

c0f453730c47f3472273b6a89b16136b9127a3fc
Elke Burgers <elke.burgers@hotmail.com>
2020-02-07 09:46:43 -0300

    commit inicial - clock - aula 3

--- aula_1/src/components/form.js
+++ aula_1/src/components/form.js
@@ -4,21 +4,26 @@ export default class Form extends React.Component {
     // render renderiza o que estah no return, sempre:
     render() {
         return (
-            <form>
-                <p>
-                <label>Nome:</label><br/>
-                <input type="text"></input>
-                </p>
+            <div className="col-12">
+                <form className="card">
+                    <div className="card-header">
+                        <h2 className="card-title">Formulário</h2>
+                    </div>
+                    <p>
+                    <label>Nome:</label><br/>
+                    <input type="text"></input>
+                    </p>
 
-                <p>
-                <label>Github:</label><br/>
-                <input type="url"></input>
-                </p>
+                    <p>
+                    <label>Github:</label><br/>
+                    <input type="url"></input>
+                    </p>
 
-                <p>
-                <button>Enviar!</button>
-                </p>
-            </form>
+                    <p>
+                    <button>Enviar!</button>
+                    </p>
+                </form>
+            </div>
         )
     }
 }
